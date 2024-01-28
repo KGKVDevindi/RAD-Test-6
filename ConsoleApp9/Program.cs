b006cebc2d57f7635550cc9bcb680493d1fd7fa6
ADMIN <ADMIN@VISH>
2024-01-28 23:05:44 +0530

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am Learning!");
+            Console.WriteLine("It is awesome!");
+            Console.Write("Hello World!");
+            Console.Write("I will print on the same Line.");
+        }
+    }
+}
